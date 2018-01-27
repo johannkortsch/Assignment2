1fd1663d9a0a80eefca1954f017e22f2b3930aa2
johannkortsch <31849571+johannkortsch@users.noreply.github.com>
2018-01-27 21:33:00 +0200

    Grid erzeugt

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { StackNavigator } from 'react-navigation'
+import PhraseList from '../Containers/PhraseList'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  PhraseList: { screen: PhraseList },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'PhraseList',
   navigationOptions: {
     headerStyle: styles.header
   }
